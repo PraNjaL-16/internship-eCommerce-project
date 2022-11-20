edfb31520ea8df0c4486c29afdca3661028a1365
Pranjal Sagar <pranjalsagar2001@gmail.com>
2022-11-20 18:48:16 +0530

    rote updates

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -7,16 +7,6 @@ const Auth = require('../middleware/auth');
 router.route('/product/:id').get(Product.getProductDetails);
 
 router.route('/products').get(Product.getAllProducts);
-router
-  .route('/admin/products')
-  .get(Auth.isAuthenticatedUser, Auth.authorizeRoles('admin'), Product.getAdminProducts);
-router
-  .route('/admin/product/new')
-  .post(Auth.isAuthenticatedUser, Auth.authorizeRoles('admin'), Product.createProduct);
-router
-  .route('/admin/product/:id')
-  .put(Auth.isAuthenticatedUser, Auth.authorizeRoles('admin'), Product.updateProduct)
-  .delete(Auth.isAuthenticatedUser, Auth.authorizeRoles('admin'), Product.deleteProduct);
 
 router.route('/review').put(Auth.isAuthenticatedUser, Product.createReview);
 router
